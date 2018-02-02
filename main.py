077b40a524ec6880d50eb572131426e5862d028d
maximeSurmontBB <maxime.surmont@hei.yncrea.fr>
2018-02-02 12:23:58 -0500

    feat: csv files imported

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import sklearn as skl
+from matplotlib import pyplot as ppl
+
+import src.data.getDataFrames
---
+++ src/data/getDataFrames.py
@@ -0,0 +1,3 @@
+netteBad = pd.read_csv('data/raw/nettebad_test_set.csv', sep=';')
+weather_dwd = pd.read_csv('data/raw/weather_dwd_train_set.csv', sep=';')
+weather_osnabrueck = pd.read_csv('data/raw/weather_uni_osnabrueck_train_set.csv', sep=';')
