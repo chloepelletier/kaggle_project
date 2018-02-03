2627f8a15ce6804a917b3ec663b9d7032927282a
chloepelletier <chloe.pelletier@hei.fr>
2018-02-03 15:04:49 -0500

    date

--- src/data/getDataFrames.py
+++ src/data/getDataFrames.py
@@ -1,3 +1,14 @@
-netteBad = pd.read_csv('data/raw/nettebad_test_set.csv', sep=';')
+netteBad = pd.read_csv('data/raw/nettebad_train_set.csv', sep=';')
 weather_dwd = pd.read_csv('data/raw/weather_dwd_train_set.csv', sep=';')
 weather_osnabrueck = pd.read_csv('data/raw/weather_uni_osnabrueck_train_set.csv', sep=';')
+
+
+from datetime import datetime
+netteBad.date = pd.to_datetime(netteBad.date, format='%m/%d/%Y')
+print((netteBad.date[0].year))
+
+weather_dwd.date = pd.to_datetime(weather_dwd.date, format='%m/%d/%Y')
+weather_osnabrueck.date = pd.to_datetime(weather_osnabrueck.date, format='%d/%m/%Y')
+
+
+
