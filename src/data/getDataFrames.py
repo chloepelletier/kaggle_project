e6bd7ed718015fae20a082c31a054795a6b0662f
chloepelletier <chloe.pelletier@hei.fr>
2018-02-17 12:07:46 -0500

    intervals for meteo

--- src/data/getDataFrames.py
+++ src/data/getDataFrames.py
@@ -1,6 +1,6 @@
-netteBad = pd.read_csv('../../data/raw/nettebad_train_set.csv', sep=';')
-weather_dwd = pd.read_csv('../../data/raw/weather_dwd_train_set.csv', sep=';')
-weather_osnabrueck = pd.read_csv('../../data/raw/weather_uni_osnabrueck_train_set.csv', sep=';')
+netteBad = pd.read_csv('data/raw/nettebad_train_set.csv', sep=';')
+weather_dwd = pd.read_csv('data/raw/weather_dwd_train_set.csv', sep=';')
+weather_osnabrueck = pd.read_csv('data/raw/weather_uni_osnabrueck_train_set.csv', sep=';')
 
 
 #Changement type date
@@ -10,4 +10,4 @@ weather_dwd.date = pd.to_datetime(weather_dwd.date, format='%m/%d/%Y')
 weather_osnabrueck.date = pd.to_datetime(weather_osnabrueck.date, format='%d/%m/%Y')
 
 
-print(type(netteBad.sportbad_closed[0]))
+print(type(netteBad.sloop_days_since_opening[0]))
